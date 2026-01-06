70cffd0e2fc6db736f26a50fe4fc20e8d7c8a3cf
Tailan Araujo <37023867+tailanttt@users.noreply.github.com>
2026-01-06 09:46:02 -0300

    Add files via upload

--- ptp_multicast.py
+++ ptp_multicast.py
@@ -26,7 +26,7 @@ def is_radio_iface(net_connect, iface):
 
 def coletar_dados(ip, username, password):
     device = {
-        "device_type": "huawei_vrp",   # ajuste para Huawei VRP
+        "device_type": "huawei",
         "ip": ip,
         "username": username,
         "password": password,
@@ -210,4 +210,5 @@ save
 display ptp all
 display clock source
 """
+    net_connect.disconnect()
     return script
